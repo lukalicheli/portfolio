91b79dbc146fb83643f6df6cb33cabbc23be3bdd
Luka Licheli <lukalicheli@gmail.com>
2023-04-05 16:45:02 -0700

    Got the basic logo appearance done

--- portfolio/src/components/nav/Navbar.js
+++ portfolio/src/components/nav/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../nav/logo.png";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar-container">
-      <img src={logo} alt="logo"></img>
+      <img className="logo" src={logo} alt="logo"></img>
       <button>About</button>
       <button>Projects</button>
       <button>Contact</button>
