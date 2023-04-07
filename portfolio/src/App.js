5397f6a2204cf2744ad70680b854018feebbc237
Luka Licheli <lukalicheli@gmail.com>
2023-04-06 22:55:12 -0700

    Added header and about components in the folder as well as set up the header to be displayed immediately after the navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/nav/Navbar";
+import Header from "./components/about/Header";
 
 function App() {
   return (
     <div className="app-container">
       <Navbar></Navbar>
+      <Header></Header>
     </div>
   );
 }
