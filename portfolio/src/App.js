9c6c0d66b6282ac03c1c390fc6788c772a0c442c
Luka Licheli <lukalicheli@gmail.com>
2023-04-06 23:36:48 -0700

    Added an about me section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/nav/Navbar";
 import Header from "./components/about/Header";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="app-container">
       <Navbar></Navbar>
       <Header></Header>
+      <About></About>
     </div>
   );
 }
---
+++ portfolio/src/components/about/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      <p className="about-me-text">
+        As a full stack developer, I specialize in creating user-friendly
+        interfaces that are visually appealing and intuitive. My expertise
+        includes React, JavaScript, and various libraries and APIs. When I code
+        on my free time, I like to make complex games such as Durak or the
+        simple favorites like tic tac toe. Check out some of my work!
+      </p>
+    </div>
+  );
+}
+
+export default About;
