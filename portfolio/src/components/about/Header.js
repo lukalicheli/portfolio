89c09545d8d6c2d079ee4efd8382d84bd69980cb
Luka Licheli <lukalicheli@gmail.com>
2023-04-10 04:14:59 -0700

    Changed up the font and sizes

--- portfolio/src/components/about/Header.js
+++ portfolio/src/components/about/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Desk from "../about/desk.png";
+import "./Header.css";
 
 function Header() {
   return (
     <div className="header-container">
+      <h1 className="full-name">Luka Licheli</h1>
       <img src={Desk} alt="desk"></img>
     </div>
   );
