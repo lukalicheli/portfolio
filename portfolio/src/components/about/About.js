119c14944a9a0e5a6ccd4e1e75d7ce8aebd87a7b
Luka Licheli <lukalicheli@gmail.com>
2023-04-10 00:16:58 -0700

    Fixed the marigns, font-size, padding, and line-height on the about me section

--- portfolio/src/components/about/About.js
+++ portfolio/src/components/about/About.js
@@ -4,8 +4,9 @@ import "./About.css";
 function About() {
   return (
     <div className="about-container">
-      <p className="about-me-text">
-        As a full stack developer, I specialize in creating user-friendly
+      <h1 className="about-me-text title">Full Stack Web Developer</h1>
+      <p className="about-me-text description">
+       I specialize in creating user-friendly
         interfaces that are visually appealing and intuitive. My expertise
         includes React, JavaScript, and various libraries and APIs. When I code
         on my free time, I like to make complex games such as Durak or the
