e26a6deae61a299f1f7b9ddb90f0d505068f59ea
Luka Licheli <lukalicheli@gmail.com>
2023-04-10 00:50:50 -0700

    Testing out extension for auto formatting on save

--- portfolio/src/components/about/About.js
+++ portfolio/src/components/about/About.js
@@ -6,11 +6,11 @@ function About() {
     <div className="about-container">
       <h1 className="about-me-text title">Full Stack Web Developer</h1>
       <p className="about-me-text description">
-       I specialize in creating user-friendly
-        interfaces that are visually appealing and intuitive. My expertise
-        includes React, JavaScript, and various libraries and APIs. When I code
-        on my free time, I like to make complex games such as Durak or the
-        simple favorites like tic tac toe. Check out some of my work!
+        I specialize in creating user-friendly interfaces that are visually
+        appealing and intuitive. My expertise includes React, JavaScript, and
+        various libraries and APIs. When I code on my free time, I like to make
+        complex games such as Durak or the simple favorites like tic tac toe.
+        Check out some of my work!
       </p>
     </div>
   );
