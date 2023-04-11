ce10530debd469098d213dcc0f191742f031352a
Luka Licheli <lukalicheli@gmail.com>
2023-04-10 21:05:46 -0700

    Finished styling the footer.

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Footer.css";
 import { GrReactjs } from "react-icons/gr";
+import { AiOutlineMail, AiFillGithub, AiFillPhone } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -10,7 +12,12 @@ function Footer() {
         Pushing boundaries with every bug, obstacle and challenge.
       </p>
 
-      <div className="footer-icons"></div>
+      <div className="footer-icons">
+        <FaLinkedinIn className="contact-icon" size={25}></FaLinkedinIn>
+        <AiFillGithub className="contact-icon" size={25}></AiFillGithub>
+        <AiOutlineMail className="contact-icon" size={25}></AiOutlineMail>
+        <AiFillPhone className="contact-icon" size={25}></AiFillPhone>
+      </div>
       <div className="made-with-react">
         {" "}
         <p>Made with</p>
