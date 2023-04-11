825c56cd42147f580dad2ff7f1a8d879851cdca6
Luka Licheli <lukalicheli@gmail.com>
2023-04-10 19:02:18 -0700

    Updated the todos for the project

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -9,8 +9,6 @@ import Games from "./components/games/Games";
 // Make a card that shows the projects.
 // Make 2 of the cards a sliding game and the tic tac toe game.
 // The third one have it as durak
-
-// Write a message positioned underneath the cards that also has a link to contact page
 // Make a footer with blue background
 // In footer include all the links to ur linkedin, github, email
 // Make a contact page
